62a27afdd59691ec6cd6ca7cd3785d830fbbf544
mvandemheen892 <122574367+mvandemheen892@users.noreply.github.com>
2023-01-20 10:29:59 -0800

    Minor Change

--- DominantColor.py
+++ DominantColor.py
@@ -4,19 +4,6 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 cap = cv.VideoCapture(0)
 
-def maxAreaCnt(cnt):
-    areaMax=0
-    areaInd=0
-    for i in range(0,len(cnt)):
-        area = cv.contourArea(cnt[i])
-        if area >= areaMax:
-            areaMax=area
-            areaInd=i
-            continue
-        else:
-            continue
-    return areaInd       
-
 def find_histogram(clt):
     """
     create a histogram with k clusters
@@ -48,42 +35,19 @@ countdown=0
 while(1):
     # Take each frame
     _, frame = cap.read()
-    # Convert BGR to HSV
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    # define range of blue color in HSV
-    lower_HSV = np.array([81,100,100])
-    upper_HSV = np.array([103,255,255])
-
-    lower_BGR= np.array([146,120,19])
-    upper_BGR= np.array([255,199,110])
-    # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_HSV, upper_HSV)
-    # Bitwise-AND mask and original image
-    res = cv.bitwise_and(frame,frame, mask= mask)
-    
-    #BoundingBoxStuff
-    contours,hierarchy = cv.findContours(mask, 1, 2)
-    if len(contours)==0:
-        continue
-    else:
-        cnt = contours[maxAreaCnt(contours)]
-
-    #cv.drawContours(res,[cnt],-1,(0,0,255),2)
-    
-    x,y,w,h = cv.boundingRect(cnt)
-    cv.rectangle(res,(x,y),(x+w,y+h),(0,255,0),2)
-
     #dominantColor
+    crop=frame[105:255,195:445]
     if countdown == 0:
-        crop=frame[105:255,195:445]
         img = cv.cvtColor(crop, cv.COLOR_BGR2RGB)
         img = img.reshape((img.shape[0] * img.shape[1],3))
         clt = KMeans(n_clusters=1) #cluster number
         clt.fit(img)
         hist = find_histogram(clt)
         bar = plot_colors2(hist, clt.cluster_centers_)
+        bar = cv.cvtColor(bar, cv.COLOR_RGB2BGR)
         cv.imshow("bar",bar)
-        countdown = 500
+    
+        countdown = 100
     cv.imshow("crop",crop)
     countdown=countdown-1
     #cv.imshow('frame',frame)
